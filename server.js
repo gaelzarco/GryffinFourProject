03529c5d09fa337e0affa403ffd335ac0652248a
cbibbs94 <89940267+cbibbs94@users.noreply.github.com>
2022-03-22 12:29:16 -0700

    set up landing route for server page and inital folders to work with

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-//The start of our wonderous app
+//Dependencies
+const express = require('express')
+
+//Configs and App Initilization
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+//Routes
+app.get('/', (req, res) => {
+    res.send('MadLibbing is on the way! Hold YA horses')
+})
+
+app.listen(PORT, () => {
+    console.log("Ooh snaps! We're live folks!!")
+})
