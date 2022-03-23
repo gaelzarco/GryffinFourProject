d3d56ecd3807a0f8a84a7962cbc75f0e22a31ef7
cbibbs94 <89940267+cbibbs94@users.noreply.github.com>
2022-03-23 14:47:56 -0700

    added possible data schema for madlibs

---
+++ models/madlibs.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose
+
+//madlib data schema
+const madlibSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String, 
+        required: true,
+        default: 'Anonymous'
+    },
+    genre: {
+        type: Schema.Types.ObjectId,
+        ref: 'Genre'
+    }
+})
+
+const Madlib = mongoose.model('Madlib', madlibSchema)
+module.exports = Madlib
