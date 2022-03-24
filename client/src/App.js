c119ee1c26d29c6522bd52f32c60c9137d1febae
gael <76548719+gaelzarco@users.noreply.github.com>
2022-03-23 19:55:14 -0700

    extra styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react'
 import MadlibHome from './components/MadlibHome';
 
@@ -15,8 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>WELCOME TO MILESTONE 2</h1>
-      {value}
+      <h1>WELCOME TO MADLIB MADNESS</h1>
       <MadlibHome />
     </div>
   );
--- client/src/components/MadlibHome.js
+++ client/src/components/MadlibHome.js
@@ -1,4 +1,10 @@
-export default function MadlibHome(props) {
+export default function MadlibHome() {
+    const renderMadList = () => {
+        if(category) {
+            
+        }
+    }
+
     return(
         <div>
             <h2>Pick a category to start!</h2>
@@ -8,6 +14,22 @@ export default function MadlibHome(props) {
             <div className="divCategory" id="div4">Category 4</div>
             <div className="divCategory" id="div5">Category 5</div>
             <div className="divCategory" id="div6">Category 6</div>
+
+            <footer>
+                <h3>Created by</h3>
+                <div>
+                <p>CJ</p>
+                </div>
+                <div>
+                <p>Chris</p>
+                </div>
+                <div>
+                <p>Gael</p>
+                </div>
+                <div>
+                <p>Jo</p>
+                </div>
+            </footer>
         </div>
     )
 }
