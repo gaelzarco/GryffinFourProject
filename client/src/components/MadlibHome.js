cc766eefd2b81f081a5f989d17cafdb61cf24a4a
gael <76548719+gaelzarco@users.noreply.github.com>
2022-03-23 19:23:06 -0700

    category styling

--- client/src/components/MadlibHome.js
+++ client/src/components/MadlibHome.js
@@ -1,7 +1,13 @@
 export default function MadlibHome(props) {
     return(
         <div>
-            <h1>Home page is being rendered successfully</h1>
+            <h2>Pick a category to start!</h2>
+            <div className="divCategory" id="div1">Category 1</div>
+            <div className="divCategory" id="div2">Category 2</div>
+            <div className="divCategory" id="div3">Category 3</div>
+            <div className="divCategory" id="div4">Category 4</div>
+            <div className="divCategory" id="div5">Category 5</div>
+            <div className="divCategory" id="div6">Category 6</div>
         </div>
     )
 }
