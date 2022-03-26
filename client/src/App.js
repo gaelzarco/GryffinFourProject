e879884a128863e69b180e9eb2362bfe3109f449
Joleena Velez <joleenavelez@gmail.com>
2022-03-26 13:42:00 -0700

    Script

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react'
-import MadlibHome from './components/MadlibHome';
+import MadlibHome from './components/madlibhome';
 
 function App() {
   const [value, setValue] = useState('')
