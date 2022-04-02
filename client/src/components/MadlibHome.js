98b74abf7ee0ca27c4a7a3fb02d94effa5b2ae44
gael <76548719+gaelzarco@users.noreply.github.com>
2022-04-01 23:00:19 -0700

    removed a console log oops. comments added

--- client/src/components/MadlibHome.js
+++ client/src/components/MadlibHome.js
@@ -3,13 +3,20 @@ import MadLib from "./MadLib"
 
 function MadlibHome(props) {
     const [ listView, setListView ] = useState([])
+    // state used for the madlibs in the user selected category
     const data = props.data
+    // sets props.data to variable
     
     const renderCatList = () => {
         if (data) {
+            // checks to see if props.data exists
             return data.map((category, index) => {
+                // maps through array of objects and inecrements index number
                 return (
-                    <div className='category' id={category.name} key={index} onClick={(e) => {renderMadList(e.target.id)}} style={{backgroundImage: `url(${category.img})`}}>
+                    // returns div for each category and assigns values to attributes
+                    <div className='category' id={category.name} key={index} onClick={(e) => {renderMadList(e.target.id)
+                    // when each div is clicked, renderMadList is called and is passed the event target id (category.name)
+                    }} style={{backgroundImage: `url(${category.img})`}}>
                         {category.name}
                     </div>
                 )
@@ -18,18 +25,23 @@ function MadlibHome(props) {
     }
 
     const renderMadList = (target) => {
+        // e.target.id is passed from onClick event in renderCatList and is received as target
         return data.filter((category) => {
+            // filters through props.data to find the category.name that matches the id (category.name) that the user clicks on
             if (category.name === target) return true
+            // returns true if its a match
             else return false
+            // returns false if category.name does not match id and continues iterating over array
         }).map((category) => {
+            // after the category the user selects is found in data, it is then mapped over and sets useState to the madlibs in the selected category
              return setListView(category.madlibs)
+             // this listView state is then used in the component's return statement
             }
         )
     }
 
     const renderMadLib = (e) => {
         let target = e.target.id
-        
 
         return (
                 <MadLib target={target} />
@@ -37,8 +49,11 @@ function MadlibHome(props) {
     }
 
     return(
-        <div key='reactBuggin'>
+        <div key='category'>
             {listView.map((madlib, index) => {
+                // the listView state, which is the user's selected madlib category, is mapped over to return each madlib as a list item and is assigned its respective attributes. This list view is rendered above the category divs.
+                // renderMadLib is assigned to an onClick event for each list item and is supposed to render MadLib.js component
+                // renderMadLib is not currently functional.
                 return <li key={index} id={madlib._id} onClick={(e) => {renderMadLib(e)}}>{madlib.name}</li>
             })}
             <div className="wrapper">
